bd52af6a8a33049d14fd092147d654e629f6bdd4
Samuel Sope <sopesamuel@gmail.com>
2022-11-12 10:54:23 +0100

    Header

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import {BrowserRouter as Router , Routes, Route} from 'react-router-dom'
-import Home from './pages/Home';
-import Blog from './pages/Blog';
-import About from './pages/About';
-import Features from './pages/Features';
-import HelpCenter from './pages/HelpCenter';
-import Login from './pages/Login';
-import SignUp from './pages/SignUp';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Blog from "./pages/Blog";
+import About from "./pages/About";
+import Features from "./pages/Features";
+import HelpCenter from "./pages/HelpCenter";
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
+import Header from "./components/Header";
 function App() {
   return (
-    <>
     <Router>
-      <Routes>
-        <Route path = '/' element={<Home/>}/>
-        <Route path = '/blog' element={<Blog/>}/>
-        <Route path = '/about' element={<About/>}/>
-        <Route path = '/features' element={<Features/>}/>
-        <Route path = '/help-center' element={<HelpCenter/>}/>
-        <Route path = '/login' element={<Login/>}/>
-        <Route path = '/signUp' element={<SignUp/>}/>
-      </Routes>
+      <div className="flex flex-col justify-between h-screen">
+        <Header />
+
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/blog" element={<Blog />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/features" element={<Features />} />
+            <Route path="/help-center" element={<HelpCenter />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signUp" element={<SignUp />} />
+          </Routes>
+        </main>
+      </div>
     </Router>
-    </>
   );
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
-    <div>Header</div>
-  )
+    <header className="header">
+      <div className="logo">
+        <Link to="/">Vaccinize</Link>
+      </div>
+
+      <ul className="navbar">
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/features">Features</Link>
+        </li>
+        <li>
+          <Link to="/help-center">Help Center</Link>
+        </li>
+        <li>
+          <Link to="/blog">Blog</Link>
+        </li>
+      </ul>
+
+      <div className="main">
+        <Link to="/login" className=" btn-0 btn-secondary btn-1">
+          Login
+        </Link>
+
+        <Link to="/signUp" className="btn-0 btn-primary btn-2">
+          SignUp
+        </Link>
+      </div>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
