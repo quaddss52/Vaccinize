f8ccee66f1bf8231a78c8442311411fc6f128232
Quadds <akinpelumoyo@gmail.com>
2022-11-15 22:54:27 +0100

    updated boiler-plate

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,36 +1,67 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 function Header() {
   return (
     <header className="header">
-      <div className="logo">
-        <Link to="/">Vaccinize</Link>
-      </div>
+      <div className="container">
+        <div className="header-content">
+          <div className="logo">
+            <NavLink to="/">Vaccinize</NavLink>
+          </div>
+          <ul className="navbar">
+            <li>
+              <NavLink
+                to="/about"
+                style={({ isActive }) => ({
+                  color: isActive ? "#244b82" : "#898989",
+                })}
+              >
+                About
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/features"
+                style={({ isActive }) => ({
+                  color: isActive ? "#244b82" : "#898989",
+                })}
+              >
+                Features
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/help-center"
+                style={({ isActive }) => ({
+                  color: isActive ? "#244b82" : "#898989",
+                })}
+              >
+                Help Center
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/blog"
+                style={({ isActive }) => ({
+                  color: isActive ? "#244b82" : "#898989",
+                })}
+              >
+                Blog
+              </NavLink>
+            </li>
+          </ul>
 
-      <ul className="navbar">
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-        <li>
-          <Link to="/features">Features</Link>
-        </li>
-        <li>
-          <Link to="/help-center">Help Center</Link>
-        </li>
-        <li>
-          <Link to="/blog">Blog</Link>
-        </li>
-      </ul>
+          <div className="main">
+            <NavLink to="/login" className=" btn-0 btn-secondary btn-1">
+              Login
+            </NavLink>
 
-      <div className="main">
-        <Link to="/login" className=" btn-0 btn-secondary btn-1">
-          Login
-        </Link>
-
-        <Link to="/signUp" className="btn-0 btn-primary btn-2">
-          SignUp
-        </Link>
+            <NavLink to="/signUp" className="btn-0 btn-primary btn-2">
+              SignUp
+            </NavLink>
+          </div>
+        </div>
       </div>
     </header>
   );
